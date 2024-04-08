70779ded0681a4c7bab1df74b165fdecd5ed655e
anantamishra <mishraananta2017@gmail.com>
2024-04-08 13:14:27 +0545

    added input based addition with useState

--- src/about.jsx
+++ src/about.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function About() {
-  return <div>About</div>;
+function About() {
+  const [input1, setInput1] = useState(0);
+  const [input2, setInput2] = useState(0);
+  const [sum, setSum] = useState(0);
+
+  const handleInput1Change = (event) => {
+    setInput1(Number(event.target.value));
+  };
+
+  const handleInput2Change = (event) => {
+    setInput2(Number(event.target.value));
+  };
+
+  const calculateSum = () => {
+    setSum(input1 + input2);
+  };
+
+  return (
+    <div>
+      <input type="number" value={input1} onChange={handleInput1Change} />
+      <input type="number" value={input2} onChange={handleInput2Change} />
+      <button onClick={calculateSum}>Calculate Sum</button>
+      <div>Sum: {sum}</div>
+    </div>
+  );
 }
+
+export default About;
--- src/contact.jsx
+++ src/contact.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <Breadcrumb />
+      <Form />
+      <Address />
+      <CTASection />
+    </div>
+  );
+}
+
+const Breadcrumb = () => {
+  return <div>This is Breadcrumb internal component</div>;
+};
+export function Form() {
+  return <div>This is internal component</div>;
+}
+export function Address() {
+  return <div>This is internal component</div>;
+}
+export function CTASection() {
+  return <div>This is internal component</div>;
 }
