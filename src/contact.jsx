52926c4011fd83826976053ace5394eadf8d957a
anantamishra <mishraananta2017@gmail.com>
2024-04-09 13:26:33 +0545

    added dynamic component

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,25 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const items = [
+  {
+    text: "Google",
+    link: "https://google.com",
+  },
+  {
+    text: "Github",
+    link: "https://github.com",
+  },
+  {
+    text: "LinkedIn",
+    link: "https://linkedin.com",
+  },
+  {
+    text: "Facebook",
+    link: "https://www.facebook.com",
+  },
+  {
+    text: "Twitter",
+    link: "https://twitter.com",
+  },
+];
 
 export default function Contact() {
   return (
-    <div>
-      <Breadcrumb />
-      <Form />
-      <Address />
-      <CTASection />
+    <div style={{ display: "flex", gap: "10px" }}>
+      {items.map((item) => {
+        return <Button link={item.link} text={item.text} />;
+      })}
     </div>
   );
 }
 
-const Breadcrumb = () => {
-  return <div>This is Breadcrumb internal component</div>;
+const Button = (props) => {
+  return (
+    <Link to={props.link}>
+      <button>{props.text}</button>
+    </Link>
+  );
 };
-export function Form() {
-  return <div>This is internal component</div>;
-}
-export function Address() {
-  return <div>This is internal component</div>;
-}
-export function CTASection() {
-  return <div>This is internal component</div>;
-}
